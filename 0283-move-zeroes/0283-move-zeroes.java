308abf886eead78c12a1bf8a208a5791b23c93bd
Kishan Raj <143512962+nist-kishan@users.noreply.github.com>
2025-03-21 23:06:03 +0530

    Time: 2 ms (84.87%), Space: 45.9 MB (64.15%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0,j=0;
+        while(i<nums.length){
+            if(nums[i]!=0){
+                nums[j++]=nums[i++];
+            }
+            else{
+                i++;
+            }
+        }
+        while(j<nums.length){
+            nums[j++]=0;
+        }
+    }
+}

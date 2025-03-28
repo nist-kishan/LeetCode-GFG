88a4ec65f3c8ee99ec0a17e380a91856a752b48b
Kishan Raj <143512962+nist-kishan@users.noreply.github.com>
2025-03-29 01:03:22 +0530

    Time: 0 ms (100%), Space: 40.9 MB (71.28%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,36 +1,13 @@
 class Solution {
     public int reverse(int x) {
-        if(x==0){
-            return x;
+        long rev=0;
+        while(x!=0){
+            int rem=x%10;
+            x/=10;
+            rev=(rev*10)+rem;
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(x);
-        boolean neg = false;
-        if(sb.charAt(0)=='-'){
-            neg=true;
-            sb.deleteCharAt(0);
-        }
-        sb.reverse();
-        while(sb.charAt(0)=='0'){
-            sb.deleteCharAt(0);
-        }
-        if(neg==true){
-            sb.insert(0,'-');
-            if(sb.length()==11){
-                StringBuilder str = new StringBuilder();
-                str.append(Integer.MIN_VALUE);
-                if(sb.compareTo(str) >= 0){
-                    return 0;
-                }
-            }
-        }
-        if(sb.length()==10){
-            StringBuilder str = new StringBuilder();
-            str.append(Integer.MAX_VALUE);
-            if(sb.compareTo(str) >= 0){
-                return 0;                
-            }
-        }
-        return (Integer.valueOf(sb.toString()));
+        if(rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE)
+        return 0;
+        return (int)rev;
     }
 }

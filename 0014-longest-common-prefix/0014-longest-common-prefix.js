d1d23a27caf9bb3f37b598c3ae9521bc9d74c8e5
Kishan Raj <143512962+nist-kishan@users.noreply.github.com>
2026-09-20 12:36:36 +0530

    Time: 0 ms (100%), Space: 54.3 MB (53.12%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    for (let i = 0; i < strs[0].length; i++) {
+        let ch = strs[0][i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (
+                i >= strs[j].length ||
+                strs[j][i] !== ch
+            ) {
+                return strs[0].substring(0, i);
+            }
+        }
+    }
+
+    return strs[0];
+};

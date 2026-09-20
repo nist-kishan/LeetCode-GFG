1af4e92ceb0f00b775eae2dfcbbb022e8cfc341c
Kishan Raj <143512962+nist-kishan@users.noreply.github.com>
2026-09-20 12:24:36 +0530

    Time: 1 ms (89.16%), Space: 55.7 MB (27.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map=new Map();
+
+    for(let i=0;i<nums.length;i++){
+        const remain=target-nums[i];
+
+        if(map.has(remain)){
+            return [map.get(remain),i];
+        }
+        map.set(nums[i],i);
+    }
+
+    return [];
+};
